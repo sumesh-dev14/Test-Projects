e4434969913a822065e7967323bcc5d9a3e65961
sumesh-dev14 <sumesh1614@gmail.com>
2025-07-26 13:45:09 +0530

    mini project

---
+++ 07-colore-changer/src/script.js
@@ -0,0 +1,23 @@
+const press = document.querySelector(".square");
+
+const y = document.querySelector(".yelo");
+const btngren = document.querySelector(".gren");
+const re = document.querySelector(".reset");
+const b = document.querySelector(".blu");
+const r = document.querySelector(".red");
+
+y.addEventListener('click' , ()=>{
+    press.style.backgroundColor = 'Yellow'
+})
+btngren.addEventListener('click' , ()=>{
+    press.style.backgroundColor = 'Green'
+})
+re.addEventListener('click' , ()=>{
+    press.style.backgroundColor = '#dee2e6'
+})
+b.addEventListener('click' , ()=>{
+    press.style.backgroundColor = 'blue'
+})
+r.addEventListener('click' , ()=>{
+    press.style.backgroundColor = 'red'
+})
