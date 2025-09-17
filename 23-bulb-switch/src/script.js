ef2f76d04a4a8db47bd9118adff78b7ccd8dda89
sumesh-dev14 <sumesh1614@gmail.com>
2025-09-17 11:37:47 +0530

    update

--- 23-bulb-switch/src/script.js
+++ 23-bulb-switch/src/script.js
@@ -2,6 +2,9 @@ const switchOn = document.getElementById("on-btn")
 const switchOff = document.getElementById("off-btn")
 const bulbLight = document.getElementById("bulb-img")
 
-switchOn.addEventListener("click" () => {
-    bulbLight.classList.add("on");
+switchOn.addEventListener("click" , () =>{
+    bulbLight.src = "../images/lightbulb-fill.svg"
+})
+switchOff.addEventListener("click" , () =>{
+    bulbLight.src = "../images/lightbulb.svg"
 })
