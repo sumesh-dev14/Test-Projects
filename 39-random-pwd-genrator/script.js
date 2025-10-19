5a51cb62825aab3d078108dfe956696db28d57da
sumesh-dev14 <sumesh1614@gmail.com>
2025-10-19 13:06:20 +0530

    new project

---
+++ 39-random-pwd-genrator/script.js
@@ -0,0 +1,14 @@
+const randomBtn = document.querySelector("#pwdGen");
+
+randomBtn.addEventListener("click", function () {
+  const length = 12;
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  document.querySelector("#input").value = password;
+});
