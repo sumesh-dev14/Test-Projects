d526d303e3a4a27bdd4b128069203181fd347f7e
sumesh-dev14 <sumesh1614@gmail.com>
2025-10-27 03:29:58 +0530

    new project

---
+++ 42-To-do-app/script.js
@@ -0,0 +1,12 @@
+const input = document.getElementById('input');
+const addBtn = document.getElementById('add-btn');
+
+addBtn.addEventListener("click" , function(){
+        const inputs = input.value;
+        if(inputs == ""){
+            alert("enter a element to add");
+            return;
+        }
+        
+
+})
