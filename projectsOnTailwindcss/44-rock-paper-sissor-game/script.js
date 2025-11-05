4385d5b3a0ccb472976431567720c7d7c3b32de1
sumesh-dev14 <sumesh1614@gmail.com>
2025-11-05 18:57:22 +0530

    update

---
+++ projectsOnTailwindcss/44-rock-paper-sissor-game/script.js
@@ -0,0 +1,9 @@
+const pressRock = document.getElementById('rock');
+const pressPaper = document.getElementById('paper');
+const pressSissors = document.getElementById('scissor');
+const resultMsg = document.getElementById('after');
+const myScore = document.getElementById('my-score');
+const compScore = document.getElementById('comp-score');
+
+let MyScore 
+
