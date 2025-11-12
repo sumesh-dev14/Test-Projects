1e4af460b908df07163d05055aa15184db033534
sumesh-dev14 <sumesh1614@gmail.com>
2025-11-12 09:32:00 +0530

    update

---
+++ 44-coin-flip/script.js
@@ -0,0 +1,7 @@
+const flipCoin = document.getElementById('flip');
+const imgCont = document.getElementById('imgdiv');
+
+
+flipCoin.addEventListener("click" , ()=>{
+    imgCont.textContent = "";
+})
