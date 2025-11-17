3c401e3f2a8b4c9780d7af2b582c22419c663430
sumesh-dev14 <sumesh1614@gmail.com>
2025-11-17 18:48:17 +0530

    update

--- 44-coin-flip/script.js
+++ 44-coin-flip/script.js
@@ -1,7 +1,8 @@
-const flipCoin = document.getElementById('flip');
-const imgCont = document.getElementById('imgdiv');
+const flipCoin = document.getElementById("flip");
+const imgCont = document.getElementById("imgdiv");
 
+flipCoin.addEventListener("click", () => {
+  const random = Math.random() < 0.5 ? "heads" : "tails";
 
-flipCoin.addEventListener("click" , ()=>{
-    imgCont.textContent = "";
-})
+  imgCont.innerHTML = `<img src="44-coin-flip/heads.jpeg" alt="${random}">`;
+});
