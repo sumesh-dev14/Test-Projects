7aea41a26fa2c68aff253ef7e18ca375f5ff851c
sumesh-dev14 <sumesh1614@gmail.com>
2025-11-26 17:34:03 +0530

    new update

---
+++ courseprojects/prjOne/script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let input = document.getElementById("todo-input");
+  let addBtn = document.getElementById("add-task-btn");
+  let listItems = document.getElementById("list-items");
+
+  let task = JSON.parse(localStorage.getItem("task")) || [];
+  // the above line is used if the task are stored in local storage
+  // it will be added in the array or if we dont find any task in localstorage
+  // it will be an empty array
+
+  task.forEach((task) => readTask(task));
+
+  addBtn.addEventListener("click", () => {
+    let taskText = input.value.trim();
+    if (taskText === "") return "Enter a task to add ";
+
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      isCompleted: false,
+    };
+    task.push(newTask);
+    saveTask();
+    input.value = "";
+    console.log(task);
+  });
+
+  // reading from local storage
+  function readTask(task) {
+    const li = document.createElement("li");
+    li.className =
+      "bg-[#333333] p-2.5 mb-2.5 rounded-md flex justify-between items-center";
+    li.setAttribute("data-id", task.id);
+    if(task.isCompleted) li.classList.classList.add('completed') // adding new class after rendring the task
+    li.innerHTML = `<span>${task.text}</span>
+    <button class="bg-[#d32f2f] text-white p-1.5 rounded-md hover:bg-[#b71c1c] btnCls">delete</button>`;
+    li.addEventListener("click" , (e)=>{
+        if(e.target.tagName === 'BUTTON') return
+        task.isCompleted = !task.isCompleted
+        li.classList.toggle('isCompleted')
+        saveTask()
+    })
+    listItems.appendChild(li);
+  }
+
+  // used to add things in local storag
+  function saveTask() {
+    localStorage.setItem("task", JSON.stringify(task));
+  }
+});
